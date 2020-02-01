31033042ad5052d74e8b3e40ebae58ec4a4f3166
Dan Neame <danneame@gmail.com>
2020-02-01 10:26:23 +0000

    Switch background based on Environment type

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-content>
-      <div class="background">
+      <div class="background" :class="environmentBackground">
         <div class="background-fade"/>
       </div>
       <router-view class="foreground"/>
@@ -10,8 +10,18 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'App',
+  computed: {
+    ...mapState('filters', [
+      'environment'
+    ]),
+    environmentBackground () {
+      return `background-${this.environment}`
+    }
+  }
 }
 </script>
 
@@ -26,7 +36,7 @@ export default {
   bottom: 0
 }
 .background-fade {
-  background: rgba(0, 0, 0, 0.6)
+  background: rgba(0, 0, 0, 0.4)
 }
 .background {
   background-image: url('./assets/background.jpg');
@@ -37,5 +47,41 @@ export default {
   position: absolute;
   top: 64px;
   z-index: 1;
+}
+.background-Arctic {
+  background-image: url('./assets/backgrounds/Arctic.jpg');
+}
+.background-Coastal {
+  background-image: url('./assets/backgrounds/Coastal.jpg');
+}
+.background-Desert {
+  background-image: url('./assets/backgrounds/Desert.jpg');
+}
+.background-Forest {
+  background-image: url('./assets/backgrounds/Forest.jpg');
+}
+.background-Grassland {
+  background-image: url('./assets/backgrounds/Grassland.jpg');
+}
+.background-Hill {
+  background-image: url('./assets/backgrounds/Hill.jpg');
+}
+.background-Jungle {
+  background-image: url('./assets/backgrounds/Jungle.png');
+}
+.background-Mountain {
+  background-image: url('./assets/backgrounds/Mountain.jpg');
+}
+.background-Swamp {
+  background-image: url('./assets/backgrounds/Swamp.jpg');
+}
+.background-Underdark {
+  background-image: url('./assets/backgrounds/Underdark.jpg');
+}
+.background-Underwater {
+  background-image: url('./assets/backgrounds/Underwater.jpg');
+}
+.background-Urban {
+  background-image: url('./assets/backgrounds/Urban.jpg');
 }
 </style>
