513b50020d25ac56e638dff809d9294e7570ac63
Dan Neame <danneame@gmail.com>
2020-02-02 00:20:52 +0000

    Add chips next to encounter header with the encounter type (Deadly, Hard etc)

--- src/components/encounters/EncounterCard.vue
+++ src/components/encounters/EncounterCard.vue
@@ -1,6 +1,12 @@
 <template>
   <v-card class="pa-4 ma-4 mb-8">
-    <h4 color="#fff">Encounter XP: {{encounter.encounterXP}} ({{encounter.totalMonsters}} mob{{encounter.totalMonsters !== 1 ? 's' : ''}})</h4>
+    <h4 color="#fff">
+      Encounter XP: {{encounter.encounterXP}} ({{encounter.totalMonsters}} mob{{encounter.totalMonsters !== 1 ? 's' : ''}})
+      <v-chip v-if="encounter.encounterXP >= challengeXp.deadly" color="red" dark>Deadly</v-chip>
+      <v-chip v-else-if="encounter.encounterXP >= challengeXp.hard" color="orange" dark>Hard</v-chip>
+      <v-chip v-else-if="encounter.encounterXP >= challengeXp.medium" color="yellow">Medium</v-chip>
+      <v-chip v-else-if="encounter.encounterXP >= challengeXp.easy" color="green">Easy</v-chip>
+    </h4>
     <monster-card
       v-for="monster in encounter.monsters"
       :key="`${encounter.id}-${monster.name}-${monster.xp}`"
@@ -10,6 +16,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import MonsterCard from '@/components/encounters/MonsterCard'
 
 export default {
@@ -17,6 +24,9 @@ export default {
   components: {
     MonsterCard
   },
-  props: ['encounter']
+  props: ['encounter'],
+  computed: mapState('filters', [
+    'challengeXp'
+  ])
 }
 </script>
